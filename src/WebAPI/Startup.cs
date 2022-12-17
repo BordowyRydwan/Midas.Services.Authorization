b3c78bddc1abd58c9354a997e324cc50f0372fdd
Dawid Wijata <dawid.wijata@hotmail.com>
2022-12-17 17:19:45 +0100

    Remove automatic migrations

--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -218,7 +218,7 @@ public class Startup
         app.UseSwagger();
         app.UseSwaggerUI();
         app.UseHeaderPropagation();
-        app.MigrateDatabase();
+        //app.MigrateDatabase();
         app.UseAuthentication();
         app.UseHttpsRedirection();
         app.UseAuthorization();
